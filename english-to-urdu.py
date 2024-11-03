4295a6e111d86cde8a25b03515ace191ff4473dd
AnonymousGhost946 <anonymousghost946@gmail.com>
2024-11-03 00:51:31 -0400

    convert english to urdu

---
+++ english-to-urdu.py
@@ -0,0 +1,5 @@
+from deep_translator import GoogleTranslator
+
+# Translate from English to Urdu
+translated_text = GoogleTranslator(source='auto', target='ur').translate('welcome to English in a minute if you ever Garden you know weeds grow super fast they can take over a garden overnight but what does grow like a weed mean in other situations do you want to see pictures from my trip home love you but this is me with my little cousin I love you I')
+print("Translated Text:", translated_text)
